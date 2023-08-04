0fbc6eef72980ddd5f94e3e06051908fd25a41f8
madvorak <martin.dvorak@matfyz.cz>
2023-08-04 21:05:42 +0200

    Thue definition

--- Grammars/Classes/Unrestricted/Thue/Definition.lean
+++ Grammars/Classes/Unrestricted/Thue/Definition.lean
@@ -1,47 +1,48 @@
-import Mathlib.Logic.Relation
 import Mathlib.Computability.Language
 
 
-inductive Symbol (T : Type) (N : Type) -- duplicate definition
-  | terminal    (t : T) : Symbol T N -- or rather "letters"
-  | nonterminal (n : N) : Symbol T N -- or rather "markers"
+inductive Tsymb (α : Type) (β : Type)
+  | letter (a : α) : Tsymb α β
+  | marker (b : β) : Tsymb α β
 
-structure Trule (T : Type) (N : Type) where
-  inputL : List T
-  inputN : N
-  inputR : List (Symbol T N) -- probably change to monolitic input side
-  output : List (Symbol T N)
+structure Trule (α : Type) (β : Type) where
+  input : List (Tsymb α β)
+  ouput : List (Tsymb α β)
 
-structure System (T : Type) where
-  nt : Type                 -- type of nonterminals / markers
-  special : nt              -- special marker to be prepended
-  rules : List (Trule T nt) -- rewrite rules
+structure Tsys (α : Type) where -- alphabet
+  beta : Type                   -- markers
+  special : beta                -- to be prepended
+  ruleset : List (Trule α beta) -- rewrite rules
 
-variable {T : Type}
+variable {α : Type}
+
+def Tsys.initiate (h : Tsys α) (w : List α) : List (Tsymb α h.beta) :=
+  Tsymb.marker h.special :: List.map Tsymb.letter w
 
 
-def System.Transforms (h : System T) (w₁ w₂ : List (Symbol T h.nt)) : Prop :=
-  ∃ r : Trule T h.nt,
-    r ∈ h.rules ∧
-    ∃ u v : List (Symbol T h.nt),
-      w₁ = u ++ (List.map Symbol.terminal r.inputL) ++ [Symbol.nonterminal r.inputN] ++ r.inputR ++ v ∧
-      w₂ = u ++ r.output ++ v
+def Tsys.Transforms (h : Tsys α) (w₁ w₂ : List (Tsymb α h.beta)) : Prop :=
+  ∃ r : Trule α h.beta,
+    r ∈ h.ruleset ∧
+    ∃ u v : List (Tsymb α h.beta), w₁ = u ++ r.input ++ v ∧ w₂ = u ++ r.ouput ++ v
 
-def System.Derives (h : System T) : List (Symbol T h.nt) → List (Symbol T h.nt) → Prop :=
-  Relation.ReflTransGen h.Transforms -- probably add counting steps
+inductive Tsys.Derives (h : Tsys α) : List (Tsymb α h.beta) → List (Tsymb α h.beta) → ℕ → Prop
+  | refl (w : List (Tsymb α h.beta)) : h.Derives w w 0
+  | tail (u v w : List (Tsymb α h.beta)) (n : ℕ) : h.Derives u v n → h.Transforms v w → h.Derives u w n.succ
 
-def System.Language (h : System T) : Language T :=
-  setOf (fun w => h.Derives (Symbol.nonterminal h.special :: List.map Symbol.terminal w) [])
--- probably extract `(Symbol.nonterminal h.special :: List.map Symbol.terminal w)` to `h.initiate w`
+def Tsys.Language (h : Tsys α) : Language α :=
+  setOf (fun w => ∃ n : ℕ, h.Derives (h.initiate w) [] n)
 
-def IsTre (L : Language T) : Prop :=
-  ∃ h : System T, h.Language = L
+def IsThue (L : Language α) : Prop :=
+  ∃ h : Tsys α, h.Language = L
 
-/-- Is there always at most one transformation possible? -/
-def System.IsDeterministic (h : System T) : Prop :=
-  ∀ w : List T, ∀ s : List (Symbol T h.nt),
-    h.Derives (Symbol.nonterminal h.special :: List.map Symbol.terminal w) s →
-      ∀ x y : List (Symbol T h.nt), h.Transforms s x ∧ h.Transforms s y → x = y
+def Tsys.IsDeterministic (h : Tsys α) : Prop :=
+  ∀ w : List α, ∀ s : List (Tsymb α h.beta), ∀ n : ℕ, h.Derives (h.initiate w) s n →
+    ∀ x y : List (Tsymb α h.beta), h.Transforms s x ∧ h.Transforms s y → x = y
 
-def IsDet (L : Language T) : Prop :=
-  ∃ h : System T, h.Language = L ∧ h.IsDeterministic
+def IsDethue (L : Language α) : Prop :=
+  ∃ h : Tsys α, h.Language = L ∧ h.IsDeterministic
+
+def Tsys.IsDtime (h : Tsys α) (f : ℕ → ℕ) : Prop :=
+  h.IsDeterministic ∧
+  ∃ c : ℕ, ∀ w : List α, ∀ s : List (Tsymb α h.beta), ∀ n : ℕ,
+    h.Derives (h.initiate w) s n → n ≤ c * f w.length

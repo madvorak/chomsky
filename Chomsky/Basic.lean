11b9fa414e7e1b8f08155a4a83072a5bd6b6f882
Martin Dvorak <martin.dvorak@matfyz.cz>
2025-11-09 12:15:55 +0100

    unexpanders

--- Chomsky/Basic.lean
+++ Chomsky/Basic.lean
@@ -16,3 +16,23 @@ prefix:max "◩" => Sum.inl
 prefix:max "◪" => Sum.inr
 
 end notations
+
+
+section unexpanders
+
+@[app_unexpander List.map]
+def List.map_unexpand : Lean.PrettyPrinter.Unexpander
+  | `($_ $f $l) => `($(l).$(Lean.mkIdent `map) $f)
+  | _ => throw ()
+
+@[app_unexpander List.take]
+def List.take_unexpand : Lean.PrettyPrinter.Unexpander
+  | `($_ $n $l) => `($(l).$(Lean.mkIdent `take) $n)
+  | _ => throw ()
+
+@[app_unexpander List.drop]
+def List.drop_unexpand : Lean.PrettyPrinter.Unexpander
+  | `($_ $n $l) => `($(l).$(Lean.mkIdent `drop) $n)
+  | _ => throw ()
+
+end unexpanders
